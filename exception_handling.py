02022927d2a88eaafbdcb9c91a9cbe3a963f9450
정석윤 <meatsby@gmail.com>
2021-08-11 14:49:55 +0000

    예외처리

--- exception_handling.py
+++ exception_handling.py
@@ -1,29 +1,29 @@
-# # 10-1. 예외처리
-# try:
-#     print("나누기 전용 계산기입니다.")
-#     nums = []
-#     nums.append(int(input("첫 번째 숫자를 입력하세요 : ")))
-#     nums.append(int(input("두 번째 숫자를 입력하세요 : ")))
-#     # nums.append(int(nums[0] / nums[1]))
-#     print("{0} / {1} = {2}".format(nums[0], nums[1], nums[2]))
-# except ValueError:
-#     print("에러! 잘못된 값을 입력하였습니다.")
-# except ZeroDivisionError as err:
-#     print(err)
-# except Exception as err:
-#     print("알 수 없는 에러가 발생하였습니다.")
-#     print(err)  # 어떤 에러인지 알려줌
+# 10-1. 예외처리
+try:
+    print("나누기 전용 계산기입니다.")
+    nums = []
+    nums.append(int(input("첫 번째 숫자를 입력하세요 : ")))
+    nums.append(int(input("두 번째 숫자를 입력하세요 : ")))
+    # nums.append(int(nums[0] / nums[1]))
+    print("{0} / {1} = {2}".format(nums[0], nums[1], nums[2]))
+except ValueError:
+    print("에러! 잘못된 값을 입력하였습니다.")
+except ZeroDivisionError as err:
+    print(err)
+except Exception as err:
+    print("알 수 없는 에러가 발생하였습니다.")
+    print(err)  # 어떤 에러인지 알려줌
 
-# # 10-2. 에러 발생시키기
-# try:
-#     print("한 자리 숫자 나누기 전용 계산기입니다.")
-#     num1 = int(input("첫 번째 숫자를 입력하세요 : "))
-#     num2 = int(input("두 번째 숫자를 입력하세요 : "))
-#     if num1 >= 10 or num2 >= 10:  # 의도적으로 except문으로 내릴 숭 있음
-#         raise ValueError
-#     print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
-# except ValueError:
-#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+# 10-2. 에러 발생시키기
+try:
+    print("한 자리 숫자 나누기 전용 계산기입니다.")
+    num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+    num2 = int(input("두 번째 숫자를 입력하세요 : "))
+    if num1 >= 10 or num2 >= 10:  # 의도적으로 except문으로 내릴 숭 있음
+        raise ValueError
+    print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
+except ValueError:
+    print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
 
 # 10-3. 사용자 정의 예외처리
 
